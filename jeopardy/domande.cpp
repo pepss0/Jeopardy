f95b39811a241abe2755a93b1abfe224bf4ff210
maiomaio09 <09maiotucu@gmail.com>
2026-05-13 17:22:05 +0200

    domande.cpp

--- jeopardy/domande.cpp
+++ jeopardy/domande.cpp
@@ -3,44 +3,23 @@
 
 using namespace std;
 
-// --- Implementazione di DOMANDA ---
-
 Domanda::Domanda() {
-    punteggio = 0;
-    giaScelta = true; // Una domanda "vuota" non deve essere giocabile
+    giaScelta = true; 
     testo="";
-    rispostaCorretta="";
 }
 
 Domanda::Domanda(string t, string r, int p) {
     testo = t;
-    rispostaCorretta = r;
-    punteggio = p;
     giaScelta = false;
 }
 
-void Domanda::mostraDomanda() {
-    if (giaScelta) {
-        cout << "[X] Casella gia' giocata o vuota!" << endl;
+int Domanda::mostraDomanda() {
+    if (giaScelta==true) {
+       return 0;
     }
     else {
-        cout << "--- DOMANDA DA " << punteggio << " PUNTI ---" << endl;
-        cout << testo << endl;
-        giaScelta = true;
+        return testo;
+        giaScelta=true;
     }
 }
 
-void Domanda::mostraRisposta()
-{
-    cout << "--- RISPOSTA DA " << punteggio << " PUNTI ---" << endl;
-    cout << rispostaCorretta << endl;
-}
-
-int Domanda::getPunteggio() {
-    return punteggio;
-}
-
-string Domanda::getRispostaCorretta()
-{
-    return rispostaCorretta;
-}
